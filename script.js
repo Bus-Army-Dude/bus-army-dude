5dc4b9ab30d369922956dfcfa8ec3a81931e9a5c
Bus Army Dude <busarmydude@gmail.com>
2025-01-19 11:16:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -145,8 +145,8 @@ setInterval(updateCountdown, 1000); // Update countdown every second
 // RedNote Shoutouts
 const redNoteShoutouts = {
     accounts: [
-        { username: '63893018335', fans: '20,000', nickname: 'Basge502', bio: 'No introduction yet', profilePic: 'rednote/basge502.webp', userId: '67853e92000000000801ee5d', token: 'ABy8b3MUGXD9mTh_DRUWGuurbnUrwIMswnddTO9cI1Gjg%3D' },
-        { username: 'busarmydude', fans: '15', nickname: 'River Kritzar', bio: 'https://bus-army-dude.github.io/bus-army-dude/', profilePic: 'rednote/busarmydude.webp', userId: '6784c343000000000803cd8c', token: 'ABArWuAlrqd-0XrSwW8448qhsXeAp_QX9ZiVo8H17' },
+        { username: '63893018335', isVerified: false, fans: '20,000', nickname: 'Basge502', bio: 'No introduction yet', profilePic: 'rednote/basge502.webp', userId: '67853e92000000000801ee5d', token: 'ABy8b3MUGXD9mTh_DRUWGuurbnUrwIMswnddTO9cI1Gjg%3D' },
+        { username: 'busarmydude', isVerified: false, fans: '15', nickname: 'River Kritzar', bio: 'https://bus-army-dude.github.io/bus-army-dude/', profilePic: 'rednote/busarmydude.webp', userId: '6784c343000000000803cd8c', token: 'ABArWuAlrqd-0XrSwW8448qhsXeAp_QX9ZiVo8H17' },
         // Add more RedNote creators as needed
     ],
     lastUpdatedTime: '2025-01-19T10:25:00', // Manually set the last updated date and time
@@ -172,6 +172,7 @@ const redNoteShoutouts = {
                 <div class="rednote-creator-info">
                     <div class="rednote-creator-header">
                         <h3>${account.nickname}</h3>
+                        ${account.isVerified ? '<img src="rednotecheck.png" alt="Verified" class="rednote-verified-badge">' : ''}
                     </div>
                     <p class="rednote-creator-username">${account.username}</p>
                     <p class="rednote-creator-bio">${account.bio || ''}</p>
