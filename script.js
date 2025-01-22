8c4e8865cad8f811818c70e26330248c3b7d7309
Bus Army Dude <busarmydude@gmail.com>
2025-01-22 09:23:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -389,7 +389,7 @@ youtubeShoutouts.init();
 
 // Manually set the last updated date and time (example in EST timezone)
 const lastUpdatedDate = "Wed, Jan 22, 2025";  // Set the date here (Day of the Week, Month, Day, Year)
-const lastUpdatedTime = "9:22:33 AM";    // Set the time here (12-hour format)
+const lastUpdatedTime = "9:22 AM";    // Set the time here (12-hour format)
 
 // Combine the date and time into a single string for parsing
 const lastUpdatedString = `${lastUpdatedDate} ${lastUpdatedTime}`;
