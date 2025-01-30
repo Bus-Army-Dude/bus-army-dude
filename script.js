1550e2f3dbb26f1640334e36212917981ec79881
Bus Army Dude <busarmydude@gmail.com>
2025-01-30 11:22:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -586,6 +586,6 @@ faqQuestions.forEach((question) => {
 });
 
  // Get the current year
-    const currentYear = new Date().getFullYear();
-    // Set the current year in the footer
-    document.getElementById('current-year').textContent = currentYear;
+        const currentYear = new Date().getFullYear();
+        // Set the current year in the footer
+        document.getElementById('current-year').textContent = currentYear;
