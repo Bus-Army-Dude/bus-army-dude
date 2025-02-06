c76b0a86d8c0db7bcbb32d722497c1ddb4e49b99
Bus Army Dude <busarmydude@gmail.com>
2025-02-06 11:43:55 -0500

    Update events.js

--- events.js
+++ events.js
@@ -8,13 +8,13 @@ class EventManager {
         // Example events structure with more detailed information
         this.events = [
             {
-                title: "Bus Army Dude's Birtday",
-                startDate: new Date('2025-02-27T10:00:00Z'),
+                title: "Bus Army Dude's Birthday",
+                startDate: new Date('2025-02-27T22:00:00Z'),
                 endDate: new Date('2025-02-28T02:00:00Z'),
-                description: 'just chatting on twitch',
+                description: 'Just Chatting with my viewers on my birthday',
                 location: 'Twitch',
-                link: 'https://www.twitch.tv/busarmydude',
-                type: 'stream' // or 'community'
+                link: 'https://twitch.tv/busarmydude',
+                type: 'community'
             }
         ];
 
