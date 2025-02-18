1314f80b49f68436cb22b8ba96040dc4211d999d
Bus Army Dude <busarmydude@gmail.com>
2025-02-17 21:15:03 -0500

    Update deviceInfo.js

--- deviceInfo.js
+++ deviceInfo.js
@@ -1,18 +1,27 @@
-function getMobileOperatingSystem() {
+function getOperatingSystem() {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
     console.log('User Agent:', userAgent);
 
-    if (/android/i.test(userAgent)) {
-        console.log('Detected Android OS');
+    if (/Windows NT/i.test(userAgent)) {
+        return 'Windows';
+    }
+
+    if (/Mac OS X/i.test(userAgent)) {
+        return 'macOS';
+    }
+
+    if (/Android/i.test(userAgent)) {
         return 'Android';
     }
 
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        console.log('Detected iOS');
         return 'iOS';
     }
 
-    console.log('OS unknown');
+    if (/Linux/i.test(userAgent)) {
+        return 'Linux';
+    }
+
     return 'unknown';
 }
 
@@ -20,24 +29,29 @@ function getDeviceModel() {
     const userAgent = navigator.userAgent;
     let model = 'unknown';
 
-    if (getMobileOperatingSystem() === 'Android') {
+    if (/Android/i.test(userAgent)) {
         const androidModelMatch = userAgent.match(/\((.*?)\)/);
         if (androidModelMatch && androidModelMatch.length > 1) {
             model = androidModelMatch[1].split(';')[1].trim();
         }
-    } else if (getMobileOperatingSystem() === 'iOS') {
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         const iOSModelMatch = userAgent.match(/\((.*?)\)/);
         if (iOSModelMatch && iOSModelMatch.length > 1) {
             model = iOSModelMatch[1].split(';')[0].trim();
         }
+    } else if (/Windows NT/i.test(userAgent)) {
+        model = 'Windows PC';
+    } else if (/Mac OS X/i.test(userAgent)) {
+        model = 'Mac';
+    } else if (/Linux/i.test(userAgent)) {
+        model = 'Linux PC';
     }
 
-    console.log('Detected Device Model:', model);
     return model;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const os = getMobileOperatingSystem();
+    const os = getOperatingSystem();
     const model = getDeviceModel();
     
     console.log(`Operating System: ${os}, Device Model: ${model}`);
