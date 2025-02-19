bfd19a801ab702d73442b7cdf2643b791754023e
Bus Army Dude <busarmydude@gmail.com>
2025-02-19 14:49:35 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -5,9 +5,3 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'G-E0RSQS1MS7');
-
-// Loading the analytics script dynamically
-const analyticsScript = document.createElement('script');
-analyticsScript.async = true;
-analyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-E0RSQS1MS7';
-document.head.appendChild(analyticsScript);
