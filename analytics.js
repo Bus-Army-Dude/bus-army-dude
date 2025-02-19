61faf640673960fc10c0ab11f9d566b085efbf8d
Bus Army Dude <busarmydude@gmail.com>
2025-02-19 14:45:57 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,6 +1,13 @@
+// Google Analytics (moved to external JS file)
 window.dataLayer = window.dataLayer || [];
 function gtag() {
     dataLayer.push(arguments);
 }
 gtag('js', new Date());
 gtag('config', 'G-E0RSQS1MS7');
+
+// Loading the analytics script dynamically
+const analyticsScript = document.createElement('script');
+analyticsScript.async = true;
+analyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-E0RSQS1MS7';
+document.head.appendChild(analyticsScript);
