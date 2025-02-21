a59240935a5e549ab5135b87aaaea8d244f5cac3
Bus Army Dude <busarmydude@gmail.com>
2025-02-21 15:25:15 -0500

    Update deviceInfo.js

--- deviceInfo.js
+++ deviceInfo.js
@@ -2,37 +2,30 @@ function getOperatingSystem() {
     const userAgent = navigator.userAgent;
     let os = 'unknown';
 
-    // Detect iOS devices (iPhone, iPad, iPod)
+    // Detect iOS/macOS
     if (/iPhone|iPad|iPod/i.test(userAgent)) {
         os = 'iOS';
-    }
-    // Detect macOS devices
-    else if (/Macintosh/i.test(userAgent)) {
+    } else if (/Macintosh/i.test(userAgent)) {
         os = 'macOS';
     }
+
     // Detect Windows
     else if (/Windows NT/i.test(userAgent)) {
         os = 'Windows';
     }
+
+    // Detect Linux
+    else if (/Linux/i.test(userAgent)) {
+        os = 'Linux';
+    }
+
     // Detect Android
     else if (/Android/i.test(userAgent)) {
         os = 'Android';
     }
-    // Detect Linux (but exclude Android)
-    else if (/Linux/i.test(userAgent) && !/Android/i.test(userAgent)) {
-        os = 'Linux'; // Make sure it's not Android
-    }
-    
+
     return os;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const os = getOperatingSystem(); // This function gets the OS
-    const osInfoElement = document.querySelector('.os-info');
-
-    if (osInfoElement) {
-        // Set the content of the element to just the OS name
-        osInfoElement.textContent = `Operating System: ${os}`;
-    }
-});
-
+// Update only the operating system part of the text (without "Operating System:" label)
+document.getElementById("os").textContent = getOperatingSystem();
