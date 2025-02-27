daafa8caea7b260b7cf27b72086f42af3d5378e8
Bus Army Dude <busarmydude@gmail.com>
2025-02-26 20:09:50 -0500

    Update creatorshoutouts.js

--- creatorshoutouts.js
+++ creatorshoutouts.js
@@ -277,7 +277,7 @@ const youtubeShoutouts = {
                     </div>
                     <p class="youtube-creator-bio">${account.bio || ''}</p>
                     <p class="youtube-subscriber-count">${account.subscribers} Subscribers</p>
-                    <a href="https://youtube.com/@${account.username}" target="_blank" class="youtube-visit-profile">
+                    <a href="https://youtube.com/@{account.username}" target="_blank" class="youtube-visit-profile">
                         Visit Channel
                     </a>
                 </div>
