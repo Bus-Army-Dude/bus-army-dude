ff2f9c6947bbf4997ecd32bf9b65ef7b4eb87326
Bus Army Dude <busarmydude@gmail.com>
2025-03-12 11:42:55 -0400

    Update creatorshoutouts.js

--- creatorshoutouts.js
+++ creatorshoutouts.js
@@ -22,13 +22,34 @@ const tiktokShoutouts = {
         { username: 'jerridonthelot', isVerified: false, followers: '267', nickname: 'Jerrid on the Lot', bio: 'Your friendly neighborhood Car Salesman and Boy Dad', profilePic: 'images/jerridonthelot.jpeg' },
         { username: 'officalbusarmydude', isVerified: false, followers: '52', nickname: 'Bus Army Dude', bio: 'https://bus-army-dude.github.io/bus-army-dude/index.html', profilePic: 'images/busarmydude.jpg' },
         // Add more shoutouts here...
-    ],
+     ],
   lastUpdatedTime: '2025-03-11T07:28:57', // Manually set the last updated date and time
   regionAvailability: {
-    US: false,
-    UK: true,
-    CA: false,
-    // Add more regions as needed
+    AD: true, AE: true, AF: true, AG: true, AI: true, AL: true, AM: true, AO: true, AQ: true, AR: true, AS: true, AT: true, AU: true, AW: true, AX: true, AZ: true, 
+    BA: true, BB: true, BD: true, BE: true, BF: true, BG: true, BH: true, BI: true, BJ: true, BL: true, BM: true, BN: true, BO: true, BQ: true, BR: true, BS: true, BT: true, BV: true, BW: true, BY: true, BZ: true, 
+    CA: false, CC: true, CD: true, CF: true, CG: true, CH: true, CI: true, CK: true, CL: true, CM: true, CN: true, CO: true, CR: true, CU: true, CV: true, CW: true, CX: true, CY: true, CZ: true, 
+    DE: true, DJ: true, DK: true, DM: true, DO: true, DZ: true, 
+    EC: true, EE: true, EG: true, EH: true, ER: true, ES: true, ET: true, 
+    FI: true, FJ: true, FK: true, FM: true, FO: true, FR: true, 
+    GA: true, GB: true, GD: true, GE: true, GF: true, GG: true, GH: true, GI: true, GL: true, GM: true, GN: true, GP: true, GQ: true, GR: true, GT: true, GU: true, GW: true, GY: true, 
+    HK: true, HM: true, HN: true, HR: true, HT: true, HU: true, 
+    ID: true, IE: true, IL: true, IM: true, IN: true, IO: true, IQ: true, IR: true, IS: true, IT: true, 
+    JE: true, JM: true, JO: true, JP: true, 
+    KE: true, KG: true, KH: true, KI: true, KM: true, KN: true, KP: true, KR: true, KW: true, KY: true, KZ: true, 
+    LA: true, LB: true, LC: true, LI: true, LK: true, LR: true, LS: true, LT: true, LU: true, LV: true, LY: true, 
+    MA: true, MC: true, MD: true, ME: true, MF: true, MG: true, MH: true, MK: true, ML: true, MM: true, MN: true, MO: true, MP: true, MQ: true, MR: true, MS: true, MT: true, MU: true, MV: true, MW: true, MX: true, MY: true, MZ: true, 
+    NA: true, NC: true, NE: true, NF: true, NG: true, NI: true, NL: true, NO: true, NP: true, NR: true, NU: true, NZ: true, 
+    OM: true, 
+    PA: true, PE: true, PF: true, PG: true, PH: true, PK: true, PL: true, PM: true, PN: true, PR: true, PT: true, PW: true, PY: true, 
+    QA: true, 
+    RE: true, RO: true, RS: true, RU: true, RW: true, 
+    SA: true, SB: true, SC: true, SD: true, SE: true, SG: true, SH: true, SI: true, SJ: true, SK: true, SL: true, SM: true, SN: true, SO: true, SR: true, SS: true, ST: true, SV: true, SX: true, SY: true, SZ: true, 
+    TC: true, TD: true, TF: true, TG: true, TH: true, TJ: true, TK: true, TL: true, TM: true, TN: true, TO: true, TR: true, TT: true, TV: true, TW: true, TZ: true, 
+    UA: true, UG: true, UM: true, US: false, UY: true, UZ: true, 
+    VA: true, VC: true, VE: true, VG: true, VI: true, VN: true, VU: true, 
+    WF: true, WS: true, 
+    YE: true, YT: true, 
+    ZA: true, ZM: true, ZW: true
   },
   apiKey: 'f40a00183b5441eab5fe9e02d4498bf6', // Replace with your ipgeolocation.io API key
   init() {
@@ -83,10 +104,21 @@ const tiktokShoutouts = {
     lastUpdatedElement.textContent = `Last Updated: ${lastUpdatedDate}`;
   },
   async getUserRegion() {
+    const cachedRegion = localStorage.getItem('userRegion');
+    const cachedTime = localStorage.getItem('userRegionTime');
+    const currentTime = Date.now();
+
+    if (cachedRegion && cachedTime && (currentTime - cachedTime < this.cacheDuration)) {
+      return cachedRegion;
+    }
+
     try {
       const response = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${this.apiKey}`);
       const data = await response.json();
-      return data.country_code2; // Return the 2-letter country code
+      const region = data.country_code2;
+      localStorage.setItem('userRegion', region);
+      localStorage.setItem('userRegionTime', currentTime);
+      return region;
     } catch (error) {
       console.error('Error fetching geolocation data:', error);
       return 'US'; // Default to 'US' if there's an error
