aa8a8c498a3cc862a5b36398805ee01c5fa9abf1
Bus Army Dude <busarmydude@gmail.com>
2025-03-17 19:32:32 -0400

    Create register-sw.js

---
+++ register-sw.js
@@ -0,0 +1,12 @@
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js')
+            .then(function(registration) {
+                console.log('ServiceWorker registration successful');
+            })
+            .catch(function(err) {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
