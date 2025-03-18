c6d609dbe8c6cdf136041c5305ea8b161336a67f
Bus Army Dude <busarmydude@gmail.com>
2025-03-18 00:42:48 -0400

    Update globalSettings.js

--- globalSettings.js
+++ globalSettings.js
@@ -8,7 +8,7 @@ class GlobalSettings {
     loadSettings() {
         const defaultSettings = {
             darkMode: true,
-            fontSize: 16,
+            fontSize: 14,
             lastUpdate: Date.now()
         };
         const savedSettings = localStorage.getItem('websiteSettings');
