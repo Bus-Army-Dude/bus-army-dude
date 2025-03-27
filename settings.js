0c7990596e4a74737970ce9d55b633fd0476fc59
Bus Army Dude <busarmydude@gmail.com>
2025-03-27 08:08:05 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -233,7 +233,7 @@ document.addEventListener('DOMContentLoaded', () => {
     settingsManager.setMaintenanceModeManually(false);
 
     // Manually set profile status
-    settingsManager.setProfileStatusManually('idle');
+    settingsManager.setProfileStatusManually('online');
 });
 
 // Function to accept cookies and hide the banner
