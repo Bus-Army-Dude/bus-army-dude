96d6c86becd4166054ea8fcfee64610fc93a3f25
Bus Army Dude <busarmydude@gmail.com>
2025-03-28 18:18:05 -0400

    Update app.js

--- assest/js/app.js
+++ assest/js/app.js
@@ -292,7 +292,7 @@ export const updateWeather = (lat, lon) => {
                             </span>
                         </div>
                         <p class="label-1">${date.getDate()} ${module.monthNames[date.getMonth()]}</p>
-                        <p class="label-1">${module.weekDayNames[date.getUTCDay()]}</p>
+                        <p class="label-1">${module.weekDayNames[date.getDay()]}</p>
                     `;
                     forecastSection.querySelector("[data-forecast-list]").appendChild(li);
                 }
