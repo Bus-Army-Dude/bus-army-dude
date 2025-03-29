b892e0f2a6e503271ecb0bea30ff38e9b3c14943
Bus Army Dude <busarmydude@gmail.com>
2025-03-29 12:51:39 -0400

    Update api.js

--- assest/js/api.js
+++ assest/js/api.js
@@ -1,37 +1,37 @@
 'use strict';
 
-const apiKey = "511c0d53e786d6e701870951d85c605d";
+const apiKey= "511c0d53e786d6e701870951d85c605d";
 
-export const fetchData = (URL, callback) => {
+export const fetchData = (URL,callback)=>{
     fetch(`${URL}&appid=${apiKey}`)
-        .then(res => res.json())
-        .then(data => callback(data));
-};
+    .then(res=>res.json())
+    .then(data=>callback(data))
+}
 
-export const url = {
-    currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+export const url ={
+    currentWeather(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`
     },
-    forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`;
+    forecast(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`
     },
-    airPollution(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}`;
+    airPollution(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon`
     },
-    reverseGeo(lat, lon) {
-        return `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5`;
-    },
-    /**
-     * @param {string} query search query e.g. :"london" , "New York"
-     */
-    geo(query) {
-        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    reverseGeo(lat,lon){
+        return `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon&limit=5`
     },
     /**
-     * @param {string} zipCode postal code e.g. :"43402"
-     * @param {string} countryCode ISO 3166 country code e.g.: "US"
+     * @param {string} query search query e.g. :"london" , "New Yourk"
      */
-    zip(zipCode, countryCode) {
-        return `https://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},${countryCode}`;
+    geo(query){
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    },
+    zip(query, countryCode) {
+        if (countryCode) {
+            return `https://api.openweathermap.org/data/2.5/weather?zip=${query},${countryCode}&units=metric`;
+        } else {
+            return `https://api.openweathermap.org/data/2.5/weather?zip=${query}&units=metric`;
+        }
     }
-};
+}
