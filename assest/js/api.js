6ef268491b7788ab1e14fae3018f90c0c0e1e2f9
Bus Army Dude <busarmydude@gmail.com>
2025-03-29 13:47:51 -0400

    Update api.js

--- assest/js/api.js
+++ assest/js/api.js
@@ -1,8 +1,8 @@
 'use strict';
 
+
 const apiKey= "511c0d53e786d6e701870951d85c605d";
-const visualCrossingApiKey = 'SKHA8C7GQCK9B27932ZNW7UXM'; // Replace with your actual key
-const visualCrossingBaseUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+
 
 export const fetchData = (URL,callback)=>{
     fetch(`${URL}&appid=${apiKey}`)
@@ -10,29 +10,24 @@ export const fetchData = (URL,callback)=>{
     .then(data=>callback(data))
 }
 
-function getVisualCrossingForecastUrl(lat, lon) {
-  return `${visualCrossingBaseUrl}${lat},${lon}?unitGroup=metric&key=${visualCrossingApiKey}&include=days`;
-}
 
 export const url ={
     currentWeather(lat,lon){
-        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
     },
     forecast(lat,lon){
-        return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
     airPollution(lat,lon){
-        return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon&appid=${apiKey}`
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
     reverseGeo(lat,lon){
-        return `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon&limit=5&appid=${apiKey}`
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
     /**
-     * @param {string} query search query e.g. :"london" , "New Yourk"
+     * @param {string} query search query e.g. :"london" , "New Yourk"  
      */
     geo(query){
         return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
 }
-
-export { getVisualCrossingForecastUrl };
