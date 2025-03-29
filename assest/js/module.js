0424280227df22f268e4ef8813088f4c2bd54415
Bus Army Dude <busarmydude@gmail.com>
2025-03-28 21:31:08 -0400

    Update module.js

--- assest/js/module.js
+++ assest/js/module.js
@@ -25,15 +25,16 @@ export const monthNames = [
     "Dec"
 ];
 
-export const getDate = (dateUnix, timezone) => {
+export const getDate = function (dateUnix, timezone) {
     const date = new Date((dateUnix + timezone) * 1000);
-    const day = date.getDate();
-    const month = monthNames[date.getMonth()];
-    return `${day} ${month}`;
+    const weekDayName = weekDayNames[date.getDay()];
+    const monthName = monthNames[date.getMonth()];
+
+    return `${weekDayName} ${date.getDate()}, ${monthName}`;
 }
 
 export const getTime = function (timeUnix, timezone) {
-    const date = new Date(timeUnix * 1000); // We are now directly using the timestamp without adding the timezone here
+    const date = new Date((timeUnix - timezone) * 1000); // Subtracting timezone here
     const hours = date.getHours();
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const period = hours >= 12 ? "PM" : "AM";
@@ -41,25 +42,39 @@ export const getTime = function (timeUnix, timezone) {
     return `${hours % 12 || 12}:${minutes} ${period}`;
 }
 
+export const getHours = function (timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date
+    .getHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12} ${period}`;
+}
+
+export const mps_to_kmh = mps => {
+    const km_per_hour = mps * 3600 / 1000;
+    return km_per_hour.toFixed(2);
+}
+
 export const aqiText = {
     1: {
         level: "Good",
-        message: "Air quality is satisfactory, and air pollution poses little or no risk."
+        message: "Air quality is considered satisfactory, and air pollution poses little or no risk"
     },
     2: {
         level: "Fair",
-        message: "Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution."
+        message: "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution"
     },
     3: {
         level: "Moderate",
-        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected"
     },
     4: {
         level: "Poor",
-        message: "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
+        message: "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects"
     },
     5: {
         level: "Very Poor",
-        message: "Health warnings of emergency conditions. The entire population is more likely to be affected."
+        message: "Health warnings of emergency conditions. The entire population is more likely to be affected"
     }
 }
