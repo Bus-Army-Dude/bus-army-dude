bfa537e53525ecc6ba80ecdc0e76fe99097929ab
Bus Army Dude <busarmydude@gmail.com>
2025-03-30 19:48:07 -0400

    Update api.js

--- assest/js/api.js
+++ assest/js/api.js
@@ -11,23 +11,24 @@ export const fetchData = (URL,callback)=>{
 }
 
 
-export const url ={
-    currentWeather(lat,lon){
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
     },
-    forecast(lat,lon){
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`;
     },
-    airPollution(lat,lon){
-        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    airPollution(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}`;
     },
-    reverseGeo(lat,lon){
-        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    reverseGeo(lat, lon) {
+        return `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5`;
     },
-    /**
-     * @param {string} query search query e.g. :"london" , "New Yourk"  
-     */
-    geo(query){
-        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    geo(query) {
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    },
+    weatherAlerts(lat, lon) {
+        // This will return alerts along with other weather data
+        return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&units=metric`;
     }
-}
+};
