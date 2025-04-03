7377a71bdeb9a617fbde4cd305f37880719ff47b
Bus Army Dude <busarmydude@gmail.com>
2025-04-03 10:25:26 -0400

    Update business-hours.js

--- business-hours.js
+++ business-hours.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
             { from: "8:00 PM", to: "8:45 PM", reason: "Busy be right back" }
         ],
         "2025-04-03": [
-            { from: "10:30 AM", to: "11:00 AM", reason: "In a meeting" }
+            { from: "10:25 AM", to: "11:00 AM", reason: "In a meeting" }
         ],
         "2025-04-03": [
             { from: "11:00 AM", to: "12:00 PM", reason: "In a meeting" }
